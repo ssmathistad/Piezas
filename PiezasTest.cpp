e4a034a9bd635d6bfdfdb3da0db5a9ab54a9af37
ssmathistad <ssmathistad@csuchico.edu>
2020-04-30 11:36:06 -0700

    Renamed tests for clarity

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -14,16 +14,44 @@ class PiezasTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-TEST(PiezasTest, initial_0_row_0_col)
+TEST(PiezasTest, initial_row_0_col_0)
 {
 	Piezas obj;
 	Piece piece_at = obj.pieceAt(0, 0);
 	ASSERT_EQ(piece_at, Blank);
 }
 
-TEST(PiezasTest, initial_0_row_3_col)
+TEST(PiezasTest, initial_state_row_0_col_3)
 {
 	Piezas obj;
 	Piece piece_at = obj.pieceAt(0, 3);
+	ASSERT_EQ(piece_at, Blank);
+}
+
+TEST(PiezasTest, initial_state_row_1_col_0)
+{
+	Piezas obj;
+	Piece piece_at = obj.pieceAt(1, 0);
+	ASSERT_EQ(piece_at, Blank);
+}
+
+TEST(PiezasTest, initial_state_row_1_col_3)
+{
+	Piezas obj;
+	Piece piece_at = obj.pieceAt(1, 3);
+	ASSERT_EQ(piece_at, Blank);
+}
+
+TEST(PiezasTest, initial_state_row_2_col_0)
+{
+	Piezas obj;
+	Piece piece_at = obj.pieceAt(2, 0);
+	ASSERT_EQ(piece_at, Blank);
+}
+
+TEST(PiezasTest, initial_state_row_2_col_3)
+{
+	Piezas obj;
+	Piece piece_at = obj.pieceAt(2, 3);
 	ASSERT_EQ(piece_at, Blank);
 }
