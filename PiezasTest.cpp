5566199851e45b7917871123eeef830497299cb3
ssmathistad <ssmathistad@csuchico.edu>
2020-05-01 04:06:25 -0700

    Tests for adding two pieces to last column

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -195,6 +195,24 @@ TEST(PiezasTest, two_pieces_in_col_0_O_invalid_move)
 	ASSERT_EQ(piece_at, X);
 }
 
+TEST(PiezasTest, two_pieces_in_col_3_X_invalid_move)
+{
+	Piezas obj;
+	obj.dropPiece(-1);
+	obj.dropPiece(0);
+	Piece piece_at = obj.pieceAt(0, 3);
+	ASSERT_EQ(piece_at, O);
+}
+
+TEST(PiezasTest, two_pieces_in_col_3_O_invalid_move)
+{
+	Piezas obj;
+	obj.dropPiece(0);
+	obj.dropPiece(-1);
+	Piece piece_at = obj.pieceAt(0, 3);
+	ASSERT_EQ(piece_at, X);
+}
+
 // Testing gameState
 TEST(PiezasTest, no_moves_made_gameState)
 {
