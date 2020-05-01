aea6a98d1549e9a866b24594a406bf661dc7d050
ssmathistad <ssmathistad@csuchico.edu>
2020-05-01 13:45:33 -0700

    Removed unreachable if statements from gameState

--- Piezas.cpp
+++ Piezas.cpp
@@ -202,15 +202,7 @@ Piece Piezas::gameState()
                     
                     prev = pieceAt(i, j);
                     
-                    if (prev == X) {
-                        col_x_cur++;
-                        if (col_x_cur > x_best)
-                            x_best = col_x_cur;
-                    } else {
-                        col_o_cur++;
-                        if (col_o_cur > o_best)
-                            o_best = col_o_cur;
-                    }
+                    (prev = X) ? col_x_cur++ : col_o_cur++;
                 // Previous piece streak continue
                 } else {
                     if (prev == X) {
